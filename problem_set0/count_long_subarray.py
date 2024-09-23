a5f74809911bbab27ff3deed46ac978a2be54bde
Jeffrey Chou <jeffreychou777>
2024-09-23 21:02:46 +0800

    probset_0_solution

--- problem_set0/count_long_subarray.py
+++ problem_set0/count_long_subarray.py
@@ -4,7 +4,32 @@ def count_long_subarray(A):
     Output: count | number of longest increasing subarrays of A
     '''
     count = 0
-    ##################
-    # YOUR CODE HERE #
-    ##################
+    record_dict = {}
+    i = 0
+    j = 1
+    length = 1
+    max_key = 0
+    def record(length):
+        if length in record_dict:
+            record_dict[length] += 1
+        else:
+            record_dict[length] = 1
+
+    while j <= len(A) - 1:
+        if A[i] < A[j]:
+            length += 1
+            i += 1
+            j += 1
+            record(length)
+        else:
+            i += 1
+            j += 1
+            length = 1
+
+    for key in record_dict.keys():
+        if key > max_key:
+            max_key = key
+    
+    count = record_dict[max_key]
+
     return count
